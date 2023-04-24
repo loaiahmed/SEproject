fa53a576843932ef099e8d6030eed69335cd00d8
mahmoud225462 <103700092+mahmoud225462@users.noreply.github.com>
2023-04-24 18:21:14 +0200

    update

--- src/TestUI.java
+++ src/TestUI.java
@@ -4,8 +4,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.LinkedList;
 
-public class TestUI extends JFrame implements ActionListener {
-    Teacher teacher;
+public class TestUI extends JFrame {
+    private final Teacher teacher;
     private JTabbedPane tabbedPane1;
     private JPanel rootPanel;
     private JButton logoutButton;
@@ -15,6 +15,7 @@ public class TestUI extends JFrame implements ActionListener {
     private JComboBox comboBox2;
     private JLabel welcomeLabel;
     private JTable table2;
+    private JComboBox comboBox3;
 
     TestUI(Teacher teacher){
         this.teacher = teacher;
@@ -22,55 +23,59 @@ public class TestUI extends JFrame implements ActionListener {
         this.setSize(900, 600);
 //        this.pack();
         this.setTitle("Teacher UI");
-        logoutButton.addActionListener(this);
 
-        comboBox1.addActionListener(this);
         this.comboBox1.addItem("All Courses");
         for(Course course : this.teacher.getCourses()){
             this.comboBox1.addItem(course.toString());
         }
 
-        comboBox2.addActionListener(this);
         this.comboBox2.addItem("All Courses");
         for(Course course : this.teacher.getCourses()){
             this.comboBox1.addItem(course.toString());
         }
 
+        createTable1(teacher.getCourses().get(0),0);
+
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setVisible(true);
-    }
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        if(e.getSource() == logoutButton);
-
-        if(e.getSource() == comboBox1){
-            if(!(comboBox1.getSelectedItem() == "All Courses")){
-                Course course = (Course) comboBox1.getSelectedItem();
-                System.out.println(course);
-                createTable1(course);
+        comboBox1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(!(comboBox1.getSelectedItem() == "All Courses")){
+                    Course course = (Course) comboBox1.getSelectedItem();
+                    System.out.println(course);
+                    clearTable();
+                    createTable1(course,0);
+                }
+                else if(comboBox1.getSelectedItem() == "All Courses"){
+                    createTable1(teacher.getCourses().get(0),0);
+                }
             }
-        }
-
-        if(e.getSource() == comboBox2);
+        });
     }
 
-    public void createTable1(Course c){
-        LinkedList<Attendance> attendances = this.teacher.getCourseRelatedAttendance(c,0);
+    public void createTable1(Course course, int recordNO){
+        LinkedList<Attendance> attendances = this.teacher.getCourseRelatedAttendance(course,0);
 
-        Object[][] data = new Object[attendances.size()][3];
+        Object[][] data = new Object[attendances.size()][4];
 
         for(int j = 0; j < attendances.size(); j++){
-            data[j][0] = attendances.get(j).getAttendanceID();
-            data[j][1] = attendances.get(j).getDateOfAttendance();
-            data[j][2] = attendances.get(j).isPresent();
+            data[j][0] = attendances.get(recordNO).getAttendanceID();
+            data[j][1] = attendances.get(recordNO).getStudent().getName();
+            data[j][2] = attendances.get(recordNO).getDateOfAttendance();
+            data[j][3] = attendances.get(recordNO).isPresent();
         }
         table1.setModel(new DefaultTableModel(
-                null,
-                new String[] {"ID", "Date" , "Present"}
+                data,
+                new String[] {"ID" ,"Student" , "Date" , "Present"}
         ));
         table1.setAutoCreateRowSorter(true); // sorting of the rows on a particular column
-        table1.getColumnModel().getColumn(5).setMaxWidth(0);
-        table1.getColumnModel().getColumn(5).setMinWidth(0);
     }
+
+    public void clearTable(){
+        DefaultTableModel dm = (DefaultTableModel)table1.getModel();
+        dm.getDataVector().removeAllElements();
+        dm.fireTableDataChanged(); // notifies the JTable that the model has changed
+    }
+
 }
